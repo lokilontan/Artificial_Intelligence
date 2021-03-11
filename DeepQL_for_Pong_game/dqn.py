8bd56731c551eb1262399ed449a657bd8a4ce36d
Vlad PlugOFF <lokilontan@yandex.ru>
2021-03-10 20:45:47 -0800

    Another test commit

--- DeepQL_for_Pong_game/dqn.py
+++ DeepQL_for_Pong_game/dqn.py
@@ -49,7 +49,7 @@ class QLearner(nn.Module):
             state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
             # TODO: Given state, you should write code to get the Q value and chosen action
             print("Hello World!") #Some code for the first test commit...
-
+            print("Another Commit!")
 
 
 
